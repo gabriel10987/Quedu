eec45d0bdb2a240f13384cb7101bccea2b76132a
Gabriel Ccama <gabriel.ccama@tecsup.edu.pe>
2024-10-07 02:50:01 -0500

    AppBar created

---
+++ components/AppBar.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import Svg, { Path } from 'react-native-svg';
+
+const AppBar = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+        <View style={styles.headerContainer}>
+            <Image
+                source={require('../assets/logo-quedu.png')}
+                style={styles.logo}
+            />
+            <View style={styles.titleContainer}>
+                <Text style={styles.q}>Q</Text>
+                <Text style={styles.restOfTitle}>uedu</Text>
+            </View>
+            <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                <Ionicons name="menu" size={32} color="#C8D0CD" />
+            </TouchableOpacity>
+        </View>
+
+        <View style={styles.welcomeContainer}>
+            <Text style={styles.welcomeText}>Hola Diego</Text>
+            <Text style={styles.promptText}>¿Listo para avanzar?</Text>
+        </View>
+
+        <Wave />
+    </View>
+  );
+};
+
+const Wave = () => (
+    <Svg
+      height="80"
+      width="100%"
+      style={styles.wave}
+    >
+      <Path
+        d="M0,40 C100,100 400,0 400,80 Q520,100 400,80 L540,100 L0,100 Z"
+        fill="#fff"
+      />
+    </Svg>
+  );
+
+const styles = StyleSheet.create({
+  container: {
+    height: 200,
+    backgroundColor: "#2C1863",
+    paddingTop: 50,
+    overflow: "hidden",
+  },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 25,
+    paddingHorizontal: 45,
+  },
+  logo: {
+    width: 35,
+    height: 35,
+  },
+
+  titleContainer: {
+    fontSize: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  q: {
+    color: "#59B7E2",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  restOfTitle: {
+    color: "#C8D0CD",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+
+  welcomeText: {
+    color: "#59B7E2",
+    fontSize: 12,
+    paddingHorizontal: 45,
+  },
+  promptText: {
+    color: "#59B7E2",
+    fontSize: 12,
+    paddingHorizontal: 45,
+  },
+  wave: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+});
+
+export default AppBar;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import AppBar from '../components/AppBar';
+
+const HomeScreen = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <AppBar navigation={navigation} />
+            <View style={styles.content}>
+                <Text>¡Bienvenido a la página de inicio!</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    content: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
+  
+  export default HomeScreen;
